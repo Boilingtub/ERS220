4e2a81fa7871742ac47ab7453b10b2274c4d9d29
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2025-08-13 08:45:37 +0200

    Add files via upload

--- line_nav.vhd
+++ line_nav.vhd
@@ -4,17 +4,20 @@ use IEEE.numeric_std.all;
 
 entity line_nav is
 port (
-	s1, s2, s3, s4 : in std_logic;
+	W : in std_logic; -- s1
+	X : in std_logic; -- s2
+	Y : in std_logic;-- s3
+	Z : in std_logic; -- s4
 	S, Tl, Tr, C, St, F : out std_logic
 );
 end line_nav;
 
 architecture line_nav_arch of line_nav is
 begin
-	S <= not(s2 and s3);
-	Tl <= not((s1 or s2) and not(s3) and not(s4));
-	Tr <= not((s3 or s4) and not(s1) and not(s2));
-	St <= not(not(s1 or s2 or s3 or s4) or (not(s3) and s4 and (s2 or s1)) or (s1 and not(s2) and s3));
-	F <= not((s1 and s3 and not(s4)) or (s4 and(s1 xor s2)) or (s2 and not(s3) and s4));
-	C <= not(s1 and s2 and s3 and s4);
+	S <= not( X and Y );
+	Tl <= not( (W or X) and not(Y) and not(Z) );
+	Tr <= not( (Y or Z) and not(W) and not(X) );
+	St <= not( not(W or X or Y or Z) or ( not(Y) and Z and (W or X)) or (W and not(X) and Y) );
+	F <= not( (W and Y and not(Z)) or (Z and (W xor X) ) or (X and not(Y) and Z) );
+	C <= not( W and X and Y and Z );
 end line_nav_arch;
